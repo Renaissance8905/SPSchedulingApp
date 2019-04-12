3b5bac74a1d62a392dfccfddfd68898e371c7800
Chris Spradling <chris.spradling@mobileforming.com>
2019-04-11 18:30:56 -0700

    Initial Commit

---
+++ SPScheduling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPScheduling
+//
+//  Created by Christopher Spradling on 4/11/19.
+//  Copyright © 2019 cspantech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
